8b5b6ac3365231b256d9b3c725e995f957039429
Wandhekar Rutika Rajendra <142422795+WandhekarRutikaRajendra@users.noreply.github.com>
2023-09-01 18:31:48 +0530

    guess updated

--- guess.java
+++ guess.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.Random;
 public class guess {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int num=45;
-        int maxAttempts=5;
+        Random ran= new Random();
+        int num = ran.nextInt(1,100)+1;
+        int maxAttempts=10;
         int attempt=1;
         int score=0;
+
+        //1. Math.random();
+        //2. Random class
         
         while(attempt<=maxAttempts){
             System.out.println("-----------------------------");
@@ -13,7 +18,7 @@ public class guess {
             System.out.println("-----------------------------");
             System.out.print("Guess the number: ");
             int guess=sc.nextInt();
-            if (guess >= 1 && guess <= 100) {
+            if (guess > 0 && guess < 100) {
                 System.out.println("You entered: " + guess);
 
                 if(guess<num){ 
@@ -35,7 +40,7 @@ public class guess {
             } 
             else {
                 System.out.println("Please enter a number between 1 and 100.");
-                break;
+                continue;
             }
             
         }
